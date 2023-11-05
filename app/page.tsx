681a9e6dcdc1f1fb603355c5044d940c269ca654
elcharitas <jonathanirhodia@gmail.com>
2023-11-05 23:53:26 +0100

    style: wrap up mobile improvements

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,25 @@ import animationData from "@/assets/animation.json";
 export default function Home() {
   return (
     <div className="flex justify-evenly items-center pt-16">
-      <div className="w-[500px] h-full flex justify-end items-end">
+      <div className="hidden w-[500px] h-full md:flex justify-end items-end">
         <Lottie animationData={animationData} />
       </div>
-      <div className="w-[500px]">
+      <div className="w-[500px] px-4">
         <div className="text-stone-900 text-5xl font-medium">
           Decentralized Data Security: Your Control, Anytime, Anywhere
         </div>
-        <div className="w-96 text-neutral-600 text-xl font-normal mt-8">
+        <div className="text-neutral-600 text-xl font-normal mt-8">
           Build on blockchain for ultimate data security. Your control, access
           anywhere, no third parties.
         </div>
 
         <div className="mt-12">
-          <h2 className="font-medium text-stone-900 text-2xl ">
+          <h2 className="font-medium text-stone-900 text-2xl">
             Get notified when we launch
           </h2>
-          <div className="flex gap-2 py-8">
+          <div className="flex flex-col md:flex-row gap-2 py-8">
             <input
-              className="w-3/4 px-8 py-4 bg-neutral-100 rounded-lg shadow justify-start items-center gap-2.5 inline-flex"
+              className="w-full md:w-3/4 px-8 py-4 bg-neutral-100 rounded-lg shadow justify-start items-center gap-2.5 inline-flex"
               placeholder="Email Address"
             />
             <button className="px-8 py-4 bg-indigo-700 hover:bg-indigo-800 rounded-lg justify-center items-center inline-flex text-zinc-50 font-semibold">
