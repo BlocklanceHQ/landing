14917b5e378126c66f97dddcd9d7f023cc4b1643
elcharitas <jonathanirhodia@gmail.com>
2023-11-06 00:54:45 +0100

    style: change hero description

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
           Welcome to the future of work!
         </div>
         <div className="text-neutral-600 text-xl font-normal mt-8">
-          Introducing Blocklance, the next generation of freelancing by
-          leveraging blockchain technology to enhance security, transparency,
-          and scalability in the freelancing industry.
+          Introducing Blocklance, the next generation of freelancing, leveraging
+          blockchain technology to enhance security, transparency, and
+          scalability in the freelancing industry.
         </div>
 
         <div className="mt-12">
